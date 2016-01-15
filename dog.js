0d17977864f950aeb348469aa562721412e762db
mlopez-mcom <matthew.lopez@macys.com>
2016-01-15 13:24:44 -0800

    small dogs are awful sounding

--- dog.js
+++ dog.js
@@ -4,11 +4,11 @@ function Dog (name, breed, age, sound, imgUrl) {
     this.breed = breed;
     this.age = age;
     this.sound = sound;
-    this.imgUrl = imgUrl; 
+    this.imgUrl = imgUrl;
 }
 
 // dog Instances
-var chiquitaBanana = new Dog('Chiquita Banana', 'chihuahua', 14, 'meeeeeow','https://s-media-cache-ak0.pinimg.com/236x/ea/ce/66/eace66909626c5a34c0dfcb75286f958.jpg');
+var chiquitaBanana = new Dog('Chiquita Banana', 'chihuahua', 14, 'high pitched squeaking dog bark','https://s-media-cache-ak0.pinimg.com/236x/ea/ce/66/eace66909626c5a34c0dfcb75286f958.jpg');
 var Emy = new Dog('Emy','shepard mix', 7, 'woof woof!','http://www.yourpurebredpuppy.com/dogbreeds/photos-AB/australianshepherds-th1.jpg' );
 
 
@@ -22,7 +22,7 @@ for (i=0;i < dogArray.length; i++) {
         breed = dogArray[i].breed,
         sound = dogArray[i].sound;
         var animalDiv = document.createElement('div');
-        animalDiv.className = 'animalContainer'        
+        animalDiv.className = 'animalContainer'
         animalDiv.innerHTML = '<div class="animalContent"><img class="animalImg" src="'+img+'" /><p class="name">'+name+'</p><p class="breed">'+breed+'</p><p class="sound">'+sound+'</p></div>';
         var sectionContainer = document.getElementById('dogs');
         sectionContainer.appendChild(animalDiv);
